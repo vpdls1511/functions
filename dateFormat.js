5a5f747c7b4589b0fdbdc1e24c93534c4508e143
sosae <sosae.cafe24app.com>
2021-07-25 17:28:05 +0900

    create date formatting

--- dateFormat.js
+++ dateFormat.js
@@ -1,5 +1,13 @@
 const dateFormatting = (date) => {
+    const year = date.getFullYear().toString();
+    const month = date.getMonth().toString();
+    const day = date.getDate().toString();
+    const hour = date.getHours().toString();
+    const min = date.getMinutes().toString();
+    const sec = date.getSeconds().toString();
 
+    // YYYY-MM-DD HH:mm:ss
+    return year+'-'+month+'-'+day+' '+hour+':'+min+':'+sec;
 }
 
-console.log(dateFormatting(new Date()))
+console.log(dateFormatting(new Date()));
